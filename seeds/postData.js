a5a174a596673026df301d79a7f4d60102113bdc
Bradley Troll <bradleytroll@gmail.com>
2024-04-06 19:54:19 -0400

    Adds seeds files.

---
+++ seeds/postData.js
@@ -0,0 +1,22 @@
+[
+    {
+        "title": "My Tech Experience",
+        "body": "This is the body of post 1",
+        "username": "talkJavaToMe"
+    },
+    {
+        "title": "Coding for Dummies",
+        "body": "This is the body of post 2",
+        "username": "DaCodeAh_Fanning"
+    },
+    {
+        "title": "A Nice Cup of JavaScript",
+        "body": "This is the body of post 3",
+        "username": "techGeek79"
+    },
+    {
+        "title": "I Love Coding",
+        "body": "This is the body of post 4",
+        "username": "codeSlayer98"
+    }
+];
---
+++ seeds/userData.js
@@ -0,0 +1,21 @@
+const userData = [
+    {
+        "username": "techGeek79",
+        "password": "password123"
+    },
+    {
+        "username": "talkJavaToMe",
+        "password": "password123"
+    },
+    {
+        "username": "codeSlayer98",
+        "password": "password123"
+    },
+    {
+        "username": "DaCodeAh_Fanning"   ,
+        "password": "password123"
+
+    }
+];  
+
+module.exports = userData;
